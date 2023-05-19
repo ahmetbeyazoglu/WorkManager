6617953292f18f26b211afd14cc72d5638679d47
herpestes <ahmetbeyazoglu@gmail.com>
2023-05-19 22:33:50 +0300

    WorkManager

--- app/src/main/java/com/herpestes/workmanager/MainActivity.kt
+++ app/src/main/java/com/herpestes/workmanager/MainActivity.kt
@@ -2,10 +2,29 @@ package com.herpestes.workmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.work.*
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val data = Data.Builder().putInt("intKey",1).build()
+
+        val constraints = Constraints.Builder()
+            //.setRequiredNetworkType(NetworkType.CONNECTED)
+            .setRequiresCharging(false)
+            .build()
+
+        val myWorkRequest : WorkRequest = OneTimeWorkRequestBuilder<RefreshDatabase>()
+            .setConstraints(constraints)
+            .setInputData(data)
+            //.setInitialDelay(5,TimeUnit.HOURS)
+            //.addTag("myTag")
+            .build()
+
+        WorkManager.getInstance(this).enqueue(myWorkRequest)
+
     }
 }
